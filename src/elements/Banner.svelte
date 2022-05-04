f2205eb2ad94714bf5b6c24e1eec6809ad76e139
Luke Shafer <hello@lukeshafer.com>
2022-05-03 21:13:03 -0400

    Cleaned up banner comments

--- src/elements/Banner.svelte
+++ src/elements/Banner.svelte
@@ -15,9 +15,6 @@
         font-family: 'Mulish', sans-serif;
 
         color: var(--heading-text);
-        /* font-size: 2em; */
-        /* width: 70em; */
-        /* background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><linearGradient id='gradient'><stop offset='10%' stop-color='%23F00'/><stop offset='90%' stop-color='%23fcc'/> </linearGradient><rect fill='url(%23gradient)' x='0' y='0' width='100%' height='100%'/></svg>"); */
     }
 </style>
 
@@ -32,21 +29,11 @@
     <path fill="var(--primary-r)"
         d="M-16.04-10.793s353.634 288.977 668.145 197.531c329.473-95.816 619.223-81.883 835.938-11.328l-38.3 338.7L-56.142 343.815zm0 0" />
 
-    <!-- <path id="title-text" fill="purple"
-        d="M-16.04-10.793s353.634 288.977 668.145 197.531c329.473-95.816 619.223-81.883 835.938-11.328l-38.3 338.7L-56.142 343.815zm0 0" /> -->
-
     <path fill="var(--primary-b)"
         d="M1.246 10.965s351.711 294.457 668.54 206.09c331.898-92.59 622.644-75.317 839.417-1.953l-42.375 340.011L-43.117 366.95zm0 0" />
 
     <path fill="var(--background)"
         d="M-3.445 22.68S345.078 326 666.165 243.793c336.358-86.137 629.03-62.703 846.019 15.562l-49.555 340.887L-55.328 379.574zm0 0" />
 
-    <!-- <path id="title-text" d="m737,197c354,-134 717,4 717,4" fill="transparent"/>
-
-    <text width="500"  x="25%" dominant-baseline="middle" text-anchor="middle">
-        <textPath xlink:href="#title-text">
-            ONO TURO-TURO
-        </textPath>
-    </text> -->
 </svg>
 
